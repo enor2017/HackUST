b30323d152dfbf222d5f0a4e75cf165da923bfbc
enor2017 <enor2017@163.com>
2021-04-16 02:01:02 +0800

    Add scatter chart into website.

--- lg1-monitor/src/Dashboard/PeopleChart.js
+++ lg1-monitor/src/Dashboard/PeopleChart.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import CanteenImage from '../assets/images/canteen_img.JPG';
+import img1220 from '../assets/images/1220.png';
+import img1240 from '../assets/images/1240.png';
+import img1300 from '../assets/images/1300.png';
 
+function get_img(value){
+    if(value === "1") return img1220;
+    else if(value === "2") return img1240;
+    else return img1300;
+}
 
-const PeopleChart = () => (
+const PeopleChart = (props) => (
   <div>
     <div className="card-content">
-      <img src={CanteenImage} height="400px"></img>
+      <img src={get_img(props.value)} height="400px"></img>
     </div>
   </div>
 );
--- lg1-monitor/src/Dashboard/TabGroup_Chart.js
+++ lg1-monitor/src/Dashboard/TabGroup_Chart.js
@@ -11,9 +11,9 @@ const TabGroup = () => (
         </div>
         <div className="card-content">
           <Tabs defaultActiveKey={1} id="plan-text-tabs">
-            <Tab eventKey={1} title="Yesterday"><PeopleChart /></Tab>
-            <Tab eventKey={2} title="Today (Est.)"><PeopleChart /></Tab>
-            <Tab eventKey={3} title="Tomorrow (Est.)"><PeopleChart /></Tab>
+            <Tab eventKey={1} title="Today 12:20"><PeopleChart value="1" /></Tab>
+            <Tab eventKey={2} title="Today 12:40"><PeopleChart value="2" /></Tab>
+            <Tab eventKey={3} title="Today 13:00"><PeopleChart value="3" /></Tab>
           </Tabs>
           <p className="card-footer">"Est." means some data are estimated using our program.</p>
         </div>
