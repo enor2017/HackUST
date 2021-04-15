3a157cb1c686aa863c6262fae7546356561a7233
zyy <zyy020327@163.com>
2021-04-16 00:05:06 +0800

    Update data-maker program

--- ObjectDetection/Data-Maker/main.py
+++ ObjectDetection/Data-Maker/main.py
@@ -19,38 +19,38 @@ def generate_holiday():
             # generate basic
             for hour in range(8, 20):
                 for minute in range(0, 60, 20):
-                    base_number = randint(5, 10)
+                    base_number = randint(12, 19)
                     result[(hour, minute)] = base_number
             # 8AM
             for minute in range(20, 60, 20):
                 extra_number = randint(4, 6)
-                result[(8, minute)] += extra_number
+                result[(8, minute)] += int(2 * extra_number)
             # 9AM
             for minute in range(0, 60, 20):
                 extra_number = randint(5, 7)
-                result[(9, minute)] += extra_number
+                result[(9, minute)] += int(2 * extra_number)
             # 11AM
             for minute in range(0, 60, 20):
                 extra_number = randint(minute/10, minute/5)
-                result[(11, minute)] += extra_number
+                result[(11, minute)] += int(2.8 * extra_number)
             # 12AM~2PM
             for hour in range(12, 14):
                 for minute in range(0, 60, 20):
                     extra_number = randint(20, 50 - hour * 2)
-                    result[(hour, minute)] += extra_number
+                    result[(hour, minute)] += int(2.5 * extra_number)
             # 4PM~5PM
             for minute in range(0, 60, 20):
                 extra_number = randint(minute/10 + 1, minute/5 + 2)
-                result[(16, minute)] += extra_number
+                result[(16, minute)] += int(1.9 * extra_number)
             # 5PM~6PM
             for hour in range(17, 19):
                 for minute in range(0, 60, 20):
                     extra_number = randint(18, 57 - hour * 2)
-                    result[(hour, minute)] += extra_number
+                    result[(hour, minute)] += int(2.1 * extra_number)
             # 7PM
             for minute in range(0, 60, 20):
                 extra_number = randint(12 - minute/5, 16 - minute/5)
-                result[(19, minute)] += extra_number
+                result[(19, minute)] += int(1.8 * extra_number)
 
             # output data
             for j in result.keys():
